1f1098ebe68075d061a0e2c693a18bd172d867eb
Ryuzaki <ufrshubham@gmail.com>
2020-09-08 06:58:04 +0530

    SFX and L4

--- scripts/PlayerBlue.gd
+++ scripts/PlayerBlue.gd
@@ -5,8 +5,9 @@ signal camera_shake_requested
 
 onready var black = get_parent().get_node("Black")
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if abs(get_position().distance_to(black.get_position())) < 64:
+		$AudioStreamPlayer.play()
 		emit_signal("camera_shake_requested")
 		emit_signal("black_collision")
 	
